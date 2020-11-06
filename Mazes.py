e53faa666e011418b0b56a0eede6b3e5d95c182b
Christian Bradley <chris@chrisgbradley.com>
2020-11-06 03:13:50 -0500

    modify wall system to reflect pairing and allow destruction

--- Mazes.py
+++ Mazes.py
@@ -1,12 +1,18 @@
 import random
+import queue
 from tkinter import Canvas
 
-class Maze(object):
 
+class Maze(object):
     class _Node:
+        wall_pairs = {'N': 'S',
+                      'S': 'N',
+                      'E': 'W',
+                      'W': 'E'}
+
         def __init__(self, x, y):
             self.x, self.y = x, y
-            self.n, self.e, self.s, self.w = True, True, True, True
+            self.walls = {'N': 0, 'S': 0, 'E': 0, 'W': 0}
 
         def getLocation(self):
             return {self.x, self.y}
@@ -16,10 +22,17 @@ class Maze(object):
             top_right = self.x * block_size + block_size, self.y * block_size
             btm_left = self.x * block_size, self.y * block_size + block_size
             btm_right = self.x * block_size + block_size, self.y * block_size + block_size
-            canvas.create_line(*top_left, *top_right)
-            canvas.create_line(*top_left, *btm_left)
-            canvas.create_line(*top_right, *btm_right)
-            canvas.create_line(*btm_left, *btm_right)
+
+            self.walls['N'] = canvas.create_line(*top_left, *top_right)
+            self.walls['S'] = canvas.create_line(*btm_left, *btm_right)
+            self.walls['E'] = canvas.create_line(*top_right, *btm_right)
+            self.walls['W'] = canvas.create_line(*top_left, *btm_left)
+
+        def destroy_wall(self, other, wall, canvas: Canvas):
+            canvas.delete(self.walls[wall])
+            self.walls[wall] = -1
+            canvas.delete(other.walls[self.wall_pairs[wall]])
+            other.walls[self.wall_pairs[wall]] = -1
 
     def __init__(self, length, width, method=0):
         self.GRID_LENGTH, self.GRID_WIDTH = length, width
@@ -35,11 +48,9 @@ class Maze(object):
 
     def __populateInner(self):
         # growing tree algorithm
-
         return 0
 
     def draw(self, canvas: Canvas, block_size):
         for row in self.grid:
             for node in row:
                 node.draw(canvas, block_size)
-
