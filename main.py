f3e3eb6e6ea894d6b8ff6402577bae35f610cc9b
Christian Bradley <chris@chrisgbradley.com>
2020-11-06 01:57:24 -0500

    add visual medium (tkinter canvas)

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from Mazes import Maze
+from tkinter import *
 
 
 def main():
-    maze = Maze(20, 25)
+    height = 50
+    width = 75
+    unit_size = 15
 
-    maze.print()
+    root = Tk()
+    root.title("Maze")
+    canvas = Canvas(root, width=1125, height=750, background='gray90')
+    canvas.pack()
+
+    canvas.create_line(1, 1, 1, width * unit_size)
+    canvas.create_line(width * unit_size, 1, width * unit_size, height * unit_size)
+    canvas.create_line(1, 1, 1, height)
+    canvas.create_line(1, height * unit_size, width * unit_size, height * unit_size)
+
+    maze = Maze(height, width)
+
+    root.mainloop()
 
 
 main()
